1efb1d2a7a1e3d09b2bd7113f73bc8bfa7d1308f
Romain Bsl <romain.boisselle@protonmail.com>
2018-05-07 06:51:47 +0200

    add solo database example

---
+++ solo/src/main/kotlin/com/example/demo/database/solo/SoloApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo.database.solo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoloApplication
+
+fun main(args: Array<String>) {
+  runApplication<SoloApplication>(*args)
+}
---
+++ solo/src/test/kotlin/com/example/demo/database/solo/CentralTest.kt
@@ -0,0 +1,26 @@
+package com.example.demo.database.solo
+
+import com.example.demo.database.data.repository.central.CitiesDirectoryRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [SoloApplication::class])
+class CentralTest {
+
+  @Autowired
+  lateinit var citiesDirectoryRepository: CitiesDirectoryRepository
+
+  @Test
+  fun readCentral() {
+    assertThat(citiesDirectoryRepository.findAll())
+      .isNotNull
+      .isNotEmpty
+      .hasSize(15)
+  }
+
+}
